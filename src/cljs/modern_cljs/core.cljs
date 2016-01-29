15a8e1d2766801a4a992d1173b8493c4f8b4d187
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2016-01-30 01:26:38 +0200

    First Mithril app mounted in Cljs!

--- src/cljs/modern_cljs/core.cljs
+++ src/cljs/modern_cljs/core.cljs
@@ -2,9 +2,17 @@
 (ns modern-cljs.core
   (:require [foo.bar] [m]))
 
+(defn ^:export ctrl []
+  (println "Calling major Tom")
+  (js-obj "var" (str "tosi: " (rand-int 10))))
+(defn ^:export viewer [arg]
+  (println "Seeing things")
+  (js/m "div" (.-var arg)))
+(def app (js-obj "controller" ctrl "view" viewer))
+
 (enable-console-print!)
 (println "Hello All!!!")
 (js/foo)
-(.render js/m
+(.mount js/m
   (.getElementById js/document "app")
-  (js/m "div" "jei!"))
+  app)
