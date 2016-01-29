5be105b7eb686160190ffdb708a25fb93a9b015c
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2016-01-30 01:44:56 +0200

    Added database object and its re-rendering
    Mithril has to be manually told to
    redraw, since update happens outside its purview.
    
    Events and network ops should make redraw happen
    automatically.

--- src/cljs/modern_cljs/core.cljs
+++ src/cljs/modern_cljs/core.cljs
@@ -2,13 +2,19 @@
 (ns modern-cljs.core
   (:require [foo.bar] [m]))
 
-(defn ^:export ctrl []
-  (println "Calling major Tom")
+(def db
   (js-obj "var" (str "tosi: " (rand-int 10))))
+
+(defn ^:export ctrl []
+  (println "Calling major Tom"))
 (defn ^:export viewer [arg]
   (println "Seeing things")
-  (js/m "div" (.-var arg)))
+  (js/m "div" (.-var db)))
 (def app (js-obj "controller" ctrl "view" viewer))
+
+(defn ^:export updatedb [arg]
+  (set! (.-var db) arg)
+  (.redraw js/m true))
 
 (enable-console-print!)
 (println "Hello All!!!")
